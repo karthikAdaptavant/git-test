d5c398c5530ccb4fb5016cf388c71fcff6399c47
Karthik samy <karthik.samy@anywhere.co>
2023-02-28 22:21:02 +0530

    Initial Commit

---
+++ GitTest/GitTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitTestApp.swift
+//  GitTest
+//
+//  Created by Karthik on 28/02/23.
+//
+
+import SwiftUI
+
+@main
+struct GitTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
