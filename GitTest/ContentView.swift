7bafd3159927c2504b4c6bebdc898b47fbf0a0d4
VishwaiOSDev <vishwa300499@icloud.com>
2023-03-01 14:02:32 +0530

    fix: Fixed the Hstack spacing

--- GitTest/ContentView.swift
+++ GitTest/ContentView.swift
@@ -15,6 +15,7 @@ struct ContentView: View {
     
     var body: some View {
         VStack {
+            HStack { Text("My Branch") }
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
