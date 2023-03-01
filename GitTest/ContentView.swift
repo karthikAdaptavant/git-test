50d1d0e123e0b3dc02c7bfac22eacc079be12ae6
VishwaiOSDev <vishwa300499@icloud.com>
2023-03-01 13:26:40 +0530

    feat: Implemented Searchbar iOS 16

--- GitTest/ContentView.swift
+++ GitTest/ContentView.swift
@@ -9,24 +9,21 @@ import SwiftUI
 
 // Adding changes from Alpha
 struct ContentView: View {
+    
+    @State private var searchText = ""
+    
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
             Text("Hello, world!")
+                .searchable(text: $searchText)
         }
         .padding()
     }
 }
 
-// #!
-// #2
-// #3
-// #4
-// $6
-// #7
-// $8
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
